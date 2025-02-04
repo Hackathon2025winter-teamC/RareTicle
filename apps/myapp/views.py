2cec6bd1b2ddb356c83233965e51e99f4e8d49c9
ryo <ryo.terajima.7@gmail.com>
2025-02-04 23:04:36 +0900

    login画面、index画面追加、ﾃﾞｨﾚｸﾄﾘ修正、動作確認済

--- apps/myapp/views.py
+++ apps/myapp/views.py
@@ -1,10 +1,2 @@
 from django.shortcuts import render
 from django.views import View
-
-
-class IndexView(View):
-    def get(self, request):
-        return render (request, 'login/login.html')
-    
-
-login = IndexView.as_view() #login/urls.pyで使用
--- login/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from apps.myapp.views import login  # Import the index view
-
-urlpatterns = [
-    path("", login, name="login"),  # Map the root URL to the index view
-]
