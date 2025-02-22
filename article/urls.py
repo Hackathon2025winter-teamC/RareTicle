4115e994eb06aad0a8dace60cc6ff55667791f77
Mattani07 <somamatsutani07@gmail.com>
2025-02-23 00:01:07 +0900

    templatesの配下にuserディレクトリを作成

--- article/urls.py
+++ article/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import IndexView, LoginView
 
+app_name = 'article'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('login/', LoginView.as_view(), name='login'),
